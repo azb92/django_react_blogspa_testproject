715f0286dcc0c226ef1bd826f5d060c87c427073
azb92 <ainar.zibbo@gmail.com>
2021-03-04 17:42:21 +0200

    Changed year in the footer

--- blog-ui/src/components/Footer/Footer.jsx
+++ blog-ui/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
 <div>
 <footer className="py-3 bg-grey">
-    <p className="m-0 text-dark text-center ">Copyright &copy; Django blog, 2020</p>
+    <p className="m-0 text-dark text-center ">Copyright &copy; Django blog, 2021</p>
 </footer>
 </div>
     );
